244cf106078f83d919847421a097ba574192ce80
Wellington Garcia <garciawell@gmail.com>
2020-02-07 00:46:37 -0300

    Creating rating and comments app

--- core/models.py
+++ core/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from attractions.models import Attractions
+from comments.models import Comments
+from ratings.models import Rating
 
 class TourismPoint(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField()
     aproved = models.BooleanField(default=False)
     attractions = models.ManyToManyField(Attractions)
+    comments = models.ManyToManyField(Comments)
+    ratings = models.ManyToManyField(Rating)
 
     def __str__(self):
         return self.name
