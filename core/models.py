5d1e1d93f0a8e091eb84fd0a0a377b9527064236
Wellington Garcia <garciawell@gmail.com>
2020-02-07 00:25:07 -0300

    creating apps and relationship manyToMany

--- core/models.py
+++ core/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from attractions.models import Attractions
 
 class TourismPoint(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField()
     aproved = models.BooleanField(default=False)
+    attractions = models.ManyToManyField(Attractions)
 
-    def _str_(self):
+    def __str__(self):
         return self.name
 
